44e39b93d500687a88f14802357edc0f34286075
Mara Wörle <github@m-woerle.de>
2024-08-21 11:11:24 +0200

    updated locale

--- german-locale.nix
+++ german-locale.nix
@@ -1,9 +1,17 @@
-{ ...}:
+{ ... }:
 
 {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.supportedLocales = [
+    "C.UTF-8/UTF-8"
+    "en_GB.UTF-8/UTF-8"
+    "de_DE.UTF-8/UTF-8"
+  ];
 
   i18n.extraLocaleSettings = {
+    LANGUAGE = "en_GB.UTF-8";
+    LC_ALL = "de_DE.UTF-8";
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
     LC_MEASUREMENT = "de_DE.UTF-8";
@@ -13,5 +21,6 @@
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "de_DE.UTF-8";
+    LANG = "en_GB.UTF-8";
   };
 }
--- packages/cli-utils.nix
+++ packages/cli-utils.nix
@@ -23,6 +23,7 @@ with lib;
       environment.systemPackages = with pkgs; [
         # Tools
         busybox
+        dust
         direnv
         doxygen
         figlet
