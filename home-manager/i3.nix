c32c76d9f8c9b70a4a7f3dc547a6ff27f6a4a82b
Mara Wörle <github@m-woerle.de>
2024-10-20 23:32:55 +0200

    added i3-rust config

---
+++ home-manager/i3.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  config = lib.mkIf config.i3.enable {
+    xsession.windowManager.i3 = {
+      enable = true;
+      config = {
+        bars = [
+            {
+              position = "top";
+              statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
+            }
+        ];
+      };
+    };
+    programs.i3status-rust = {
+      enable = true;
+      bars = {
+        top = {
+          blocks = [
+          {
+            block = "time";
+            interval = 60;
+            format = "%a %d/%m %k:%M %p";
+          }
+        ];
+        };
+      };
+    };
+  };
+}
